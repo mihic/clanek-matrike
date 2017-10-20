58cf6b631f874eb0609d197336cba664308d69a5
Mitja Rozman <rozman.mitj@gmail.com>
2017-10-20 12:30:19 +0200

    Repair of PrintMatrix, cleaning of matrix.cpp

--- programi/matrix.cpp
+++ programi/matrix.cpp
@@ -3,18 +3,6 @@
 #include <iomanip>
 #include "matrix.h"
 
-//code for testing
-//void PrintMatrix(Tmat &mat) {
-//  for (auto &vrstica : mat) {
-//    //std::cout << "|";
-//    for (auto &element : vrstica) {
-//      //std::cout << std::setw(8) << element;
-//    }
-//    //std::cout << "|" << std::endl;
-//  }
-//  //std::cout << std::endl;
-//}
-
 bool DimensionCheckFailed(Tmat &mat1,Tmat &mat2) {
   if(mat1.n != mat2.m){
      std::cout << "Abnormal use of multiplication." << std::endl;
@@ -32,19 +20,12 @@ void PrintMatrix(Tmat &mat){
 
   for (int i = 0; i < m; i++){
     for (int j = 0; j < n; j++){
-      //std::cout << std::setw(9) <<mat(i,j) << " | ";
+      std::cout << std::setw(9) << mat(i,j) << " | ";
     }
-    //std::cout << std::endl;
+    std::cout << std::endl;
   }
-  //std::cout << std::endl;
+  std::cout << std::endl;
 }
-
-//code for algorithms
-//Tmat newMat(int a, int b){
-//    std::vector<double> nicelni(a, 0.0);
-//    Tmat mat(b, nicelni);
-//    return mat;
-//}
 
 Tmat Transpose(Tmat &mat){
     int a = mat.m;
@@ -81,11 +62,7 @@ void MultiplicationClassicTransposedPart(Tmat &mat1, Tmat &mat2, Tmat &mat3,
                                          int b1, int b2,
                                          int c1, int c2){
 
-    //std::cout << "MCTP a1=" << a1 << " a2=" << a2 << " b1=" << b1 << " b2=" << b2 << " c1=" << c1 << " c2=" << c2 << std::endl;
-    //std::cout << " mat3.m=" << mat3.m << " mat3.n=" << mat3.n << std::endl;
-    //std::cout << " mat2.m=" << mat2.m << " mat2.n=" << mat2.n << std::endl;
-    //std::cout << " mat1.m=" << mat1.m << " mat1.n=" << mat1.n << std::endl;
-    //mat2 je transponirana
+    //mat2 is transposed
     for (int i = a1; i<a2; i+=1){
         for (int j = c1; j<c2; j+=1){
             double s = 0;
@@ -96,3 +73,26 @@ void MultiplicationClassicTransposedPart(Tmat &mat1, Tmat &mat2, Tmat &mat3,
         }
     }
 }
+
+//Legacy code
+//
+//void PrintMatrix(Tmat &mat) {
+//  for (auto &vrstica : mat) {
+//    //std::cout << "|";
+//    for (auto &element : vrstica) {
+//      //std::cout << std::setw(8) << element;
+//    }
+//    //std::cout << "|" << std::endl;
+//  }
+//  //std::cout << std::endl;
+//}
+//
+//Tmat newMat(int a, int b){
+//    std::vector<double> nicelni(a, 0.0);
+//    Tmat mat(b, nicelni);
+//    return mat;
+//}
+//std::cout << "MCTP a1=" << a1 << " a2=" << a2 << " b1=" << b1 << " b2=" << b2 << " c1=" << c1 << " c2=" << c2 << std::endl;
+//std::cout << " mat3.m=" << mat3.m << " mat3.n=" << mat3.n << std::endl;
+//std::cout << " mat2.m=" << mat2.m << " mat2.n=" << mat2.n << std::endl;
+//std::cout << " mat1.m=" << mat1.m << " mat1.n=" << mat1.n << std::endl;
